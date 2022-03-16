f596553e1b9eef46a5f94c6d933e6172adc65eae
wilgru <william.gruszka@gmail.com>
2022-03-16 15:17:59 +1100

    initial commit

---
+++ assets/js/indexScript.js
@@ -0,0 +1,59 @@
+var startGameButton = document.getElementById("start-game");
+var leaderboardListInitials = document.getElementById("leaderboard-list-initials");
+var leaderboardListTime = document.getElementById("leaderboard-list-time");
+var leaderboardListCorrect = document.getElementById("leaderboard-list-correct");
+
+var leaderboard = [];
+
+// upon loading, get any scores stored in local storage and render them from the get go
+function init() {
+    leaderboard = JSON.parse(getLocalLeaderboard());
+}
+
+// gets any saved leaderboard data from local storage and stores it in current session
+function getLocalLeaderboard() {
+    for (let i = 0; i < localStorage.length; i++) {
+        leaderboard.push(JSON.parse(localStorage[i]));
+    }
+    sortLeaderboard()
+}
+
+// sort the leaderboard object by score and then by time -- insertion sort
+function sortLeaderboard() {
+    for(let i = 1; i < leaderboard.length; i++){
+
+        //Go through the elements behind it
+        for(let j = i-1; j >= 0; j--){
+            
+            //value comparison using asc order
+            if(leaderboard[j + 1].time > leaderboard[j].time){
+                //swap
+                [leaderboard[j+1],leaderboard[j]] = [leaderboard[j],leaderboard[j + 1]];
+
+            } else if (leaderboard[j + 1].time === leaderboard[j].time) {
+                if(leaderboard[j + 1].correct < leaderboard[j].correct){
+                    //swap
+                    [leaderboard[j+1],leaderboard[j]] = [leaderboard[j],leaderboard[j + 1]];
+                }
+            }
+        }
+    }
+}
+
+// renders the leaderboard
+function renderLeaderboard() {
+    getLocalLeaderboard()
+
+    for (i = 0; i < leaderboard.length; i++) {
+        leaderboardListInitials.innerHTML = leaderboardListInitials.innerHTML + "<p>" + leaderboard[i].initials + "</p>"
+        leaderboardListTime.innerHTML = leaderboardListTime.innerHTML + "<p>" + leaderboard[i].time + "</p>"
+        leaderboardListCorrect.innerHTML = leaderboardListCorrect.innerHTML + "<p>" + leaderboard[i].correct + "</p>"
+    }
+}
+
+renderLeaderboard()
+
+//event listeners
+startGameButton.addEventListener("click", () => {
+    window.location.href = './quiz.html';
+})
---
+++ assets/js/quizScript.js
@@ -0,0 +1,301 @@
+var questionH2 = document.getElementById("question-h2");
+var choicesList = document.getElementById("choices-list");
+var saveScoreCard = document.getElementById("save-score-card");
+var choiceOutcomeP = document.getElementById("choice-outcome");
+var timeLeftP = document.getElementById("time-left");
+var progressP = document.getElementById("progress");
+var initialsForm = document.getElementById("initials-entry-form");
+
+const questions = {
+    question1: {
+        question: "How would you define an object in JavaScript?",
+        choices: {
+            choice1: "var myObj = {};",
+            choice2: "var myObj = Object.newObject();",
+            choice3: "Object.newObject(myObj);",
+            choice4: "var myObj = [];",
+        },
+        correctAnswer: "1"
+    },
+    question2: {
+        question: "What is the difference between the '==' and the '===' comparitive operators?",
+        choices: {
+            choice1: "one compares type regardless of the values, the other compares value regardless of the types",
+            choice2: "one compares type and value, while the other compares only value regardless of the types",
+            choice3: "both do the same thing, but one is the older syntax of the other",
+            choice4: "one compares type and value, the other compares size regardless of the values or thhe types",
+        },
+        correctAnswer: "2"
+    },
+    question3: {
+        question: "Which of the following is NOT considered 'falsy'?",
+        choices: {
+            choice1: "null",
+            choice2: "\"\"",
+            choice3: "'false'",
+            choice4: "!true",
+        },
+        correctAnswer: "3"
+    },
+    question4: {
+        question: "Say 'myObj' is an object variable with key value pairs defined in it, which of the following returns a list of keys in an object?",
+        choices: {
+            choice1: "myObj.keys()",
+            choice2: "Object.myObj.keys()",
+            choice3: "myObj[0]",
+            choice4: "Object.keys(myObj)",
+        },
+        correctAnswer: "4"
+    },
+}
+
+// question related variables
+var currentChoiceButtons;
+var currentQuestionIndex;
+var currentQuestion;
+var currentAnswer;
+var progress;
+
+// time related variables
+const totatTime = 60;
+var timer;
+var timeLeft = totatTime;
+
+// user's choice related variables
+var chosenChoice;
+var answereSelected = false;
+var correctChoices = 0;
+
+// functions
+// main game start function
+function startGame() {
+    currentQuestionIndex = 0;
+    currentQuestion = Object.keys(questions)[currentQuestionIndex];
+    progress = 1;
+    progressP.innerHTML = "<strong>Progress: </strong>" + progress + "/" + Object.keys(questions).length;
+    timeLeftP.innerHTML = "<strong>Time Left: </strong>" + timeLeft;
+
+    renderQuestion() // renders the first question and the buttons
+    gameClock() // starts game clock
+}
+
+// end the game
+function endGame() {
+    clearInterval(timer);
+
+    questionH2.textContent = "Quiz Finished!"
+    choicesList.style.display = "none";
+    choiceOutcomeP.textContent = "";
+    saveScoreCard.style.display = "block";
+}
+
+// handles game count down and time out 
+function gameClock() {
+    timer = setInterval( () => {
+
+        // if time is up, 
+        if (timeLeft === 0) {
+            clearInterval(timer); // stop timer
+
+            fadeButtonsOut() // fade the buttons out
+
+            questionH2.textContent = "Times out!"
+            timeLeftP.innerHTML = "<strong>Time Left: </strong> 0" 
+            answereSelected = true; // stop user form pressing any buttons
+            
+            setTimeout(() => { // call ennd of game after 2 secs
+                endGame() // call end game sequence
+            }, 2000);
+
+        } else {
+            // otherwise decrement counter and display the time left
+            timeLeft--;
+            timeLeftP.innerHTML = "<strong>Time Left: </strong>" + timeLeft
+        }
+    }, 1000)
+}
+
+// render the current question and each option as a button for the current question
+function renderQuestion() {
+    answereSelected = false;
+
+    // render the current question text
+    questionH2.innerHTML = 'Question ' + (currentQuestionIndex+1) + ': <br>' + questions[currentQuestion].question;
+
+    // empty thhe list and get the choices for the current question ready
+    choicesList.textContent = '';
+    var currentChoices = Object.values(questions[currentQuestion].choices);
+
+    // render each button, then get the button with the correct answer in it, a list of all the buttons and then give the buttons their event listeners
+    for (let i = 0; i < currentChoices.length; i++) { 
+        var newChoiceButton = document.createElement('button');
+        
+        newChoiceButton.innerText = currentChoices[i];
+        newChoiceButton.setAttribute("id", i+1);
+
+        var newChoiceLi = document.createElement('li');
+        newChoiceLi.appendChild(newChoiceButton);
+        choicesList.appendChild(newChoiceLi);
+    }
+
+    // get all buttons and add event listeners to them
+    currentChoiceButtons = document.querySelectorAll("button");
+    currentAnswer = document.getElementById(questions[currentQuestion].correctAnswer);
+    
+    renderButtonEventListeners(); // render event listeners for every button on the page currently
+
+    restoreButtonStyles() // restore all buttons style to full opacity and no colour
+}
+
+// render the next question
+function renderNextQuestion () {
+    // reset and increment variables ready for next round (next 'renderQuestion' call)
+    answereSelected = false;
+    currentQuestionIndex++;
+    currentQuestion = Object.keys(questions)[currentQuestionIndex];
+    currentAnswer = document.getElementById(questions[currentQuestion].correctAnswer);
+
+    renderQuestion() // render next question
+}
+
+// restore all buttons style to full opacity and no colour
+function restoreButtonStyles() {
+    // return each button to full transparency and colours to normal colours
+    for (let i = 0; i < currentChoiceButtons.length; i++) {
+        var button = currentChoiceButtons[i];
+
+        button.style.transition = "none";
+        button.style.opacity = "100";
+        button.style.backgroundColor = "white";
+        button.style.color = "black";
+        button.style.borderColor = "black";
+    }
+
+    // remove the outcome text
+    choiceOutcomeP.innerText = "";
+}
+
+// gray styling to every button
+function grayOutButtons() {
+    for (let i = 0; i < currentChoiceButtons.length; i++) {
+        var button = currentChoiceButtons[i];
+
+        button.style.backgroundColor = "lightgray";
+        button.style.color = "darkgray";
+        button.style.borderColor = "darkgray";
+    }
+}
+
+// green styling for the correctly selected button
+function correctGuess() {
+    grayOutButtons() // grays out all the buttons first
+
+    chosenChoice.style.backgroundColor = "green";
+    chosenChoice.style.color = "darkgreen";
+    chosenChoice.style.borderColor = "darkgreen";
+
+    choiceOutcomeP.textContent = "Correct!"
+
+    correctChoices++;
+}
+
+// red styling for the incorrectly selected button and green styling for the correct button
+function incorrectGuess() {
+    grayOutButtons() // grays out all the buttons first
+
+    timeLeft = timeLeft - 10;
+    timeLeftP.innerHTML = "<strong>Time Left: </strong>" + timeLeft;
+
+    // highlight the actual correct answer too
+    currentAnswer.style.backgroundColor = "green";
+    currentAnswer.style.color = "darkgreen";
+    currentAnswer.style.borderColor = "darkgreen";
+
+    // highligt the incorrect answer 
+    chosenChoice.style.backgroundColor = "red";
+    chosenChoice.style.color = "darkred";
+    chosenChoice.style.borderColor = "darkred";
+
+    choiceOutcomeP.textContent = "Incorrect!"
+}
+
+// fade buttons out
+function fadeButtonsOut() {
+    for (let i = 0; i < currentChoiceButtons.length; i++) {
+        var button = currentChoiceButtons[i];
+        button.style.transition = "750ms";
+        button.style.opacity = "0";
+    }
+}
+
+// check if the selected button is correct or not
+function checkChoice(event) {
+    // get the number id associatied with the button selected
+    chosenChoice = event.target;
+    var choiceNumId = chosenChoice.id;
+
+    // check if the number of the chosen button matches te number of te correct answer or not
+    if (choiceNumId === questions[currentQuestion].correctAnswer) {
+        correctGuess();
+    } else {
+        incorrectGuess();
+    }
+}
+
+// check if the use has reahced the end of the game or not
+function checkEndOfGame() {
+
+    // wait 250ms before fading out, and wait 500ms for fade out to complete before rendering next question or end of game
+    setTimeout(() => {
+        fadeButtonsOut() // fade out the buttons first
+
+        // if reached te end of the questions 
+        if (currentQuestionIndex === Object.keys(questions).length-1) {
+            setTimeout(() => {
+                endGame(); // end game sequence
+            }, 500);
+
+        } else {
+            // otherwise display the progress and then render tehe next question after 500ms
+            progress++;
+            progressP.innerHTML = "<strong>Progress: </strong>" + progress + "/" + Object.keys(questions).length;
+
+            setTimeout(() => {
+                renderNextQuestion(); // render next question
+            }, 500);
+        }
+    }, 250);
+}
+
+// render an event listener for each button on the page
+function renderButtonEventListeners() {
+    for (let i = 0; i < currentChoiceButtons.length; i++) {
+        var button = currentChoiceButtons[i];
+
+        button.addEventListener("click", (event) => {
+            if (!answereSelected) {
+                answereSelected = true;
+                checkChoice(event);
+                checkEndOfGame();
+            }
+        });
+    }
+}
+
+startGame();
+
+// event for when form is submitted
+initialsForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    var newId = localStorage.length;
+    var entry = {
+        initials: initialsForm.elements['initials'].value,
+        time: timeLeft,
+        correct: correctChoices
+    }
+
+    //put the new score into localStorage
+    localStorage.setItem( newId, JSON.stringify(entry));
+    window.location = "./index.html";
+})
